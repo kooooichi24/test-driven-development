35f314fee9491d9cca8e7b6663a474301cbae313
古川広一 <ex.kouichi1224@gmail.com>
2020-11-14 18:11:32 +0900

    第6章 テスト不足に気づいたら

---
+++ src/money/Money.java
@@ -0,0 +1,14 @@
+package money;
+
+/**
+ * Moneyクラス
+ * 貨幣の親クラス
+ */
+public class Money {
+    protected int amount;
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
+}
