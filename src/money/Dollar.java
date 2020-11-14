c346aa97102f8e6302397b0c13a80664bd69c09f
古川広一 <ex.kouichi1224@gmail.com>
2020-11-14 16:17:53 +0900

    第2章 明白な実装

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -5,12 +5,11 @@ package money;
  */
 public class Dollar {
     int amount;
-
     Dollar(int amount) {
         this.amount = amount;
     }
 
-    void times(int multiplier) {
-        amount *= multiplier;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
