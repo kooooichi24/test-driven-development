00c67fc7bc34a374980274deb123915a73668cec
古川広一 <ex.kouichi1224@gmail.com>
2020-11-16 23:49:18 +0900

    第15章 テスト任せとコンパイラ任せ

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -79,4 +79,15 @@ public class MoneyTest {
     public void testIdentityRate() {
         assertEquals(1, new Bank().rate("USD", "USD"));
     }
+
+    @Test
+    @DisplayName("$5 + 10CHF = $10 (レートが2:1の場合)をテストする")
+    public void testMixedAddition() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
 }
